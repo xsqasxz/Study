7d846bbf7ebc2afa9acab0adb0f5cd4c2c52f450
xsqasxz <34931021+xsqasxz@users.noreply.github.com>
2018-04-18 00:34:39 +0800

    学习Spring Boot第一章成果

---
+++ studyspringboot2/src/main/java/com/example/HelloworldController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Controller 是Spring MVC 注解，表示此类用于负责处理Web请求。
+ * @RequestMapping 是Strping MVC  注解， 表示如果请求路径匹配吗，被注解的方法将被调用。
+ * @ResponseBody 表示此方法返回的是文本而不是视图名称。
+ */
+@Controller
+public class HelloworldController {
+
+    @RequestMapping("/sayhello.html")
+    public @ResponseBody String say(){
+        return "Holle Spring Boot";
+    }
+
+}
