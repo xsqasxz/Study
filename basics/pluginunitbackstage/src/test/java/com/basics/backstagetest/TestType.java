82a3414e6d63b3797c4edb841fcdc2d3ccc966f9
xueshiqi <xuesq@darenloan.com>
2019-03-27 17:23:15 +0800

    对目录进行整理

---
+++ basics/pluginunitbackstage/src/test/java/com/basics/backstagetest/TestType.java
@@ -0,0 +1,17 @@
+package com.basics.backstagetest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author xueshiqi
+ * @since 2019/3/27
+ */
+public class TestType {
+
+    @Test
+    public void testStyle(){
+        assertEquals(1,1);
+    }
+}
